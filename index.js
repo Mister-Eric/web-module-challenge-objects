eb3fea2d5886b8efb5b1568217e64e3e46aa881a
Eric Montgomery <ericismyname@protonmail.com>
2021-01-21 16:53:18 -0800

    Still working on task 6 - 2 hours now

--- index.js
+++ index.js
@@ -103,7 +103,8 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   2. log the reviews array to the console to check your work
 */
 
-
+reviews.push({name: Reyna, rating: 3.5, feedback: 'this place is chill with really cool people, great for getting work done on weekdays'});
+return reviews;
 
 
 
@@ -118,7 +119,7 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(/*Your code here*/) {
+function getReviewByIndex(ohRay) {
   /*Your code here*/
 }
 
