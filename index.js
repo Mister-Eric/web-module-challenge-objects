59186a6c25549404743d46270cbb8c394f64a957
Eric Montgomery <ericismyname@protonmail.com>
2021-01-21 17:21:30 -0800

    Asked for help on task 6

--- index.js
+++ index.js
@@ -119,8 +119,8 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(array, 0) {
-  return reviews (`{name} gave the restaurant a {rating} star review, and their feedback was: {feedback}`)
+function getReviewByIndex(array) {
+  return array (`${name} gave the restaurant a ${rating} star review, and their feedback was: ${feedback}`)
 }
 console.log(getReviewByIndex(reviews, 1));
 
